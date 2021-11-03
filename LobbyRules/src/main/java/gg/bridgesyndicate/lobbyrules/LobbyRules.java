6a7ca6287b99141048947e2db28d5c7d3e968691
Vice Versa <vice@bridgesyndicate.gg>
2021-11-02 21:24:57 -0700

    -nofall -nohunger -noweather -always day -no pvp -block protection

--- LobbyRules/src/main/java/gg/bridgesyndicate/lobbyrules/LobbyRules.java
+++ LobbyRules/src/main/java/gg/bridgesyndicate/lobbyrules/LobbyRules.java
@@ -1,10 +1,15 @@
 package gg.bridgesyndicate.lobbyrules;
 
-import org.bukkit.Material;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LobbyRules extends JavaPlugin implements Listener {
@@ -13,13 +18,45 @@ public final class LobbyRules extends JavaPlugin implements Listener {
     public void onEnable() {
         System.out.println(this.getClass() + " is loading.");
         getServer().getPluginManager().registerEvents(this, this);
+        setGameRules();
     }
 
     @EventHandler
-    public void onPlayerDropItem(PlayerDropItemEvent event) {
-        Player p = event.getPlayer();
-        if (event.getItemDrop().getItemStack().getType() == Material.BOW || event.getItemDrop().getItemStack().getType() == Material.ARROW || event.getItemDrop().getItemStack().getType() == Material.GOLDEN_APPLE || event.getItemDrop().getItemStack().getType() == Material.IRON_SWORD || event.getItemDrop().getItemStack().getType() == Material.DIAMOND_PICKAXE || event.getItemDrop().getItemStack().getType() == Material.STAINED_CLAY) {
+    public void onWeatherChange(final WeatherChangeEvent e) {
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void foodChangeEvent(final FoodLevelChangeEvent event) {
+        if (event.getEntityType() == EntityType.PLAYER) {
+            final Player player = (Player) event.getEntity();
+            event.setCancelled(true);
+            player.setFoodLevel(20);
+        }
+    }
+
+    @EventHandler
+    public void entityDamageEvent(final EntityDamageEvent event) {
+        if (event.getEntityType() == EntityType.PLAYER) {
             event.setCancelled(true);
         }
+    }
+
+    @EventHandler
+    public void onBlockPlaceEvent(BlockPlaceEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockBreakEvent(BlockBreakEvent event) {
+        event.setCancelled(true);
+    }
+
+
+    private void setGameRules() {
+        Bukkit.getWorld("world").setGameRuleValue("keepInventory", "true");
+        Bukkit.getWorld("world").setGameRuleValue("naturalRegeneration", "false");
+        Bukkit.getWorld("world").setGameRuleValue("doDaylightCycle", "false");
+        Bukkit.getWorld("world").setGameRuleValue("randomTickSpeed", "0");
     }
 }
