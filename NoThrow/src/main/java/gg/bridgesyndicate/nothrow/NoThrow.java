4653ded310e81af3bd965dd0defbaadff5057b15
bdamja <1>
2022-02-07 01:16:36 +0000

    Squashed commit of the following
    commit 28af17f5 
    Author: bdamja <iceicebabyalter@gmail.com> 
    Date: Sun Feb 06 2022 18:13:50 GMT-0700 (Mountain Standard Time) 
    
        simplified nothrow
    
    
    commit 30d833cd 
    Author: bdamja <iceicebabyalter@gmail.com> 
    Date: Sun Feb 06 2022 18:13:25 GMT-0700 (Mountain Standard Time) 
    
        fixed losing items in kit customization

--- NoThrow/src/main/java/gg/bridgesyndicate/nothrow/NoThrow.java
+++ NoThrow/src/main/java/gg/bridgesyndicate/nothrow/NoThrow.java
@@ -3,12 +3,10 @@ package gg.bridgesyndicate.nothrow;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NoThrow extends JavaPlugin implements Listener {
@@ -21,9 +19,16 @@ public final class NoThrow extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent event) {
-        Player p = event.getPlayer();
-        if (event.getItemDrop().getItemStack().getType() == Material.BOW || event.getItemDrop().getItemStack().getType() == Material.ARROW || event.getItemDrop().getItemStack().getType() == Material.GOLDEN_APPLE || event.getItemDrop().getItemStack().getType() == Material.IRON_SWORD || event.getItemDrop().getItemStack().getType() == Material.DIAMOND_PICKAXE || event.getItemDrop().getItemStack().getType() == Material.STAINED_CLAY) {
-            event.setCancelled(true);
-        }
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerSplitItem(InventoryClickEvent event) {
+        if (event.isRightClick()) event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerDragItem(InventoryDragEvent event) {
+        event.setCancelled(true);
     }
 }
